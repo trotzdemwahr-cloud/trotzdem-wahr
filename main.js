d65a656d2d2687ddc639d20ab4f4c7e07f69fcd7
trotzdemwahr-cloud <lenakroemtz@gmx.de>
2026-06-29 15:50:30 +0200

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
- document.querySelectorAll('.btn').forEach(btn=>{
-   btn.addEventListener('mouseenter',()=>btn.style.transform='translateY(-2px)');
-   btn.addEventListener('mouseleave',()=>btn.style.transform='translateY(0)');
- });
-});
