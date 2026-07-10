d58b20ebb9e23c386b27d8660698c44a608a9448
trotzdemwahr-cloud <lenakroemtz@gmx.de>
2026-07-10 15:09:54 +0200

    Update workbook.js

--- workbook-v2/assets/js/workbook.js
+++ workbook-v2/assets/js/workbook.js
@@ -1,1 +1,23 @@
+/*
+======================================================
 
+trotzdem.wahr
+Workbook V2
+
+======================================================
+*/
+
+
+const workbook = {
+
+    title: "Zurück zu dir",
+
+    subtitle:
+        "Ein kostenloses Workbook über Selbstreflexion, psychologische Zusammenhänge und neue Perspektiven.",
+
+
+    steps: [
+
+    ]
+
+};
