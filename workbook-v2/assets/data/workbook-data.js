e4decf129301650188c0c5cea8ef4734648c804a
trotzdemwahr-cloud <lenakroemtz@gmx.de>
2026-07-10 15:11:37 +0200

    Update workbook-data.js

--- workbook-v2/assets/data/workbook-data.js
+++ workbook-v2/assets/data/workbook-data.js
@@ -1,1 +1,16 @@
+const workbookData = {
 
+    title: "Zurück zu dir",
+
+    subtitle:
+        "Ein kostenloses Workbook über Selbstreflexion, psychologische Zusammenhänge und neue Perspektiven.",
+
+    version: "2.0",
+
+    author: "trotzdem.wahr",
+
+    steps: [
+
+    ]
+
+};
